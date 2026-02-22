24d6f7270a16eba47b49c91cd44dbbd9eeb45eca
Felipe Alafy <git@felipealafy.net>
2026-02-22 15:18:48 -0300

    Removed AppViewModelProvider, created DatabaseModule inside of the package dependencyinjection

---
+++ app/src/main/java/net/felipealafy/studentplanner/dependencyinjection/DatabaseModule.kt
@@ -0,0 +1,41 @@
+package net.felipealafy.studentplanner.dependencyinjection
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import net.felipealafy.studentplanner.database.StudentPlannerDatabase
+import javax.inject.Singleton
+
+@Suppress("unused")
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) : StudentPlannerDatabase {
+        return Room.databaseBuilder(
+            context,
+            StudentPlannerDatabase::class.java,
+            "student_planner_db"
+        ).fallbackToDestructiveMigration().build()
+    }
+
+    @Provides
+    fun providePlannerDao(database: StudentPlannerDatabase) = database.PlannerDao()
+
+    @Provides
+    fun provideSubjectDao(database: StudentPlannerDatabase) = database.SubjectDao()
+
+    @Provides
+    fun provideClassDao(database: StudentPlannerDatabase) = database.ClassDao()
+
+    @Provides
+    fun provideExamDao(database: StudentPlannerDatabase) = database.ExamDao()
+}
