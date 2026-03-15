fddcf7ed5ba08561631680b9ea1c7f98cffacace
Felipe Alafy <git@felipealafy.net>
2026-03-15 18:21:18 -0300

    Moved function GradeInputFromAToFWithE to components.grade

--- app/src/main/java/net/felipealafy/studentplanner/ui/views/ExamCreationView.kt
+++ app/src/main/java/net/felipealafy/studentplanner/ui/views/ExamCreationView.kt
@@ -45,6 +45,7 @@ import net.felipealafy.studentplanner.datamodels.GradeStyle
 import net.felipealafy.studentplanner.datamodels.Subject
 import net.felipealafy.studentplanner.ui.components.color.chooser.ButtonWithBackgroundColor
 import net.felipealafy.studentplanner.ui.components.grade.GradeInputFromAToF
+import net.felipealafy.studentplanner.ui.components.grade.GradeInputFromAToFWithE
 import net.felipealafy.studentplanner.ui.components.grade.GradeInputFromZeroToOneHundred
 import net.felipealafy.studentplanner.ui.components.grade.GradeInputFromZeroToTen
 import net.felipealafy.studentplanner.ui.components.grade.GradeWeightInput
