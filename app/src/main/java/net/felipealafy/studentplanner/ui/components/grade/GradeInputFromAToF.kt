34ff49beca66ac4fccf868383f8c5d3a79867262
Felipe Alafy <git@felipealafy.net>
2026-03-15 18:17:01 -0300

    Moved function GradeInputFromAToF to compoenents.grade

---
+++ app/src/main/java/net/felipealafy/studentplanner/ui/components/grade/GradeInputFromAToF.kt
@@ -0,0 +1,111 @@
+package net.felipealafy.studentplanner.ui.components.grade
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import net.felipealafy.studentplanner.R
+import net.felipealafy.studentplanner.datamodels.GradeAToF
+import net.felipealafy.studentplanner.ui.theme.DarkGray
+import net.felipealafy.studentplanner.ui.theme.Typography
+import net.felipealafy.studentplanner.ui.theme.colorPallet
+import net.felipealafy.studentplanner.ui.views.getContrastingColorForText
+import net.felipealafy.studentplanner.ui.views.getForBackgroundBasedOnTitleBarColor
+
+@Composable
+fun GradeInputFromAToF(selectedColor: Long, onSelectItem: (GradeAToF) -> Unit) {
+    val textColor =
+        Color(selectedColor.getForBackgroundBasedOnTitleBarColor().getContrastingColorForText())
+    var expanded by remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .border(
+                width = 2.dp,
+                color = DarkGray,
+                shape = RoundedCornerShape(30.dp)
+            ),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            var selectedItem by remember {
+                mutableStateOf(
+                    GradeAToF.C
+                )
+            }
+            val textToDisplay = selectedItem.name
+            IconButton(
+                { expanded = !expanded },
+            ) {
+                Spacer(Modifier.padding(start = 8.dp))
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    tint = textColor,
+                    contentDescription = stringResource(R.string.grade_style_combobox)
+                )
+            }
+
+            Spacer(Modifier.padding(start = 10.dp))
+
+            Text(
+                text = textToDisplay,
+                style = Typography.labelMedium,
+                color = textColor,
+            )
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                containerColor = Color(selectedColor),
+                shape = RoundedCornerShape(30.dp)
+            ) {
+                GradeAToF.entries.forEach {
+                    DropdownMenuItem(
+                        text = {
+                            Text(
+                                text = it.name,
+                                color = textColor
+                            )
+                        },
+                        onClick = {
+                            selectedItem = it
+                            onSelectItem(it)
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun GradeInputFromAToFPreview() {
+    GradeInputFromAToF(
+        selectedColor = colorPallet[0][1]
+    ) { }
+}
--- app/src/main/java/net/felipealafy/studentplanner/ui/views/ExamCreationView.kt
+++ app/src/main/java/net/felipealafy/studentplanner/ui/views/ExamCreationView.kt
@@ -44,6 +44,7 @@ import net.felipealafy.studentplanner.R
 import net.felipealafy.studentplanner.datamodels.GradeStyle
 import net.felipealafy.studentplanner.datamodels.Subject
 import net.felipealafy.studentplanner.ui.components.color.chooser.ButtonWithBackgroundColor
+import net.felipealafy.studentplanner.ui.components.grade.GradeInputFromAToF
 import net.felipealafy.studentplanner.ui.components.grade.GradeInputFromZeroToOneHundred
 import net.felipealafy.studentplanner.ui.components.grade.GradeInputFromZeroToTen
 import net.felipealafy.studentplanner.ui.components.grade.GradeWeightInput
